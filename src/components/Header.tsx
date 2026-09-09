3a0dc9a364ee7f178fef4aced478f8309daac518
kurosiko <kurosiko@outlook.com>
2026-09-09 21:30:39 +0900

    feat: add Trail logo to site header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ export default function Header() {
 	return (
 		<header>
 			<nav aria-label="navigaion bar">
-				<a class="site-title" href="/">
-					Trail Logo
+				<a class="site-title" href="/" aria-label="Trail ホーム">
+					<img class="site-logo" src="/trail-logo.svg" alt="Trail" />
 				</a>
 				<div class="nav-links">
 					<a href="/about">About</a>
